2363e3f757439911879f3ef522b491fbee74908d
arn <arnaud@desaivres.net>
2020-11-01 14:29:13 +0100

    fix

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -1007,7 +1007,7 @@
  * Specify a Probe position as { X, Y, Z }
  */
 // Arnaud: Changed as per Mesured
-#define NOZZLE_TO_PROBE_OFFSET { -36, 0, -2.35 }
+#define NOZZLE_TO_PROBE_OFFSET { -36, 0, -2.11 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
